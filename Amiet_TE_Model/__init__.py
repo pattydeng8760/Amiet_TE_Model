b46a69275ae5c5abb75f042c827636c0da99b51e
Patrick G.C. Deng <patrickgc.deng@mail.utoronto.ca>
2025-04-30 02:44:01 -0600

    fixed naming bug in __init__.py

--- Amiet_TE_Model/__init__.py
+++ Amiet_TE_Model/__init__.py
@@ -2,4 +2,5 @@ from .calc_zerf import zerf, zerfc
 from .calc_fresnel import FresCS2
 from .calc_gammaln import gammaln
 from .calc_rdirsupTE import rdirsupTE
-from .Aimet_TE import AimetTE
+from .utils_checkdata import check_data
+from .Amiet_TE import AmietTE
